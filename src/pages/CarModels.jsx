f83e679b10b8925d1da598db50b39259abdd3e53
Khamidov-M <mardonbekkhamidov69@gmail.com>
2024-01-31 21:31:25 +0500

    comenting

--- src/pages/CarModels.jsx
+++ src/pages/CarModels.jsx
@@ -71,9 +71,6 @@ const CarModels = () => {
   const Sliced = modelsArray.slice(0, number);
   const [showbtn, setShowBtn] = useState(true);
   const another = modelsArr.elems;
-  const [sixSlice, setSixSlice] = useState(6);
-  const elems = another.slice(0, sixSlice);
-  const [btn, setbtn] = useState(true);
 
   // siling
 
@@ -115,8 +112,18 @@ const CarModels = () => {
     });
   };
 
+  const [firstindex, setfirstindex] = useState(1);
+  const [lastindex, setlastindex] = useState(6);
+
+  const lastPost = firstindex * lastindex;
+  const firstPost = lastPost - lastindex;
+
+  const slicedCards = found.slice(firstPost, lastPost);
+
   return (
     <>
+      {/* car model cards */}
+
       <div className="w-full flex justify-center">
         <div className="w-[96%] mb-10 xl:w-[1300px]">
           <div className="flex flex-col justify-between items-center mb-10 sm:flex-row">
@@ -159,6 +166,8 @@ const CarModels = () => {
           )}
         </div>
       </div>
+
+      {/* cars filtering card */}
 
       <div className="w-full flex justify-center">
         <div className="w-[96%] mb-10 xl:w-[1300px]">
@@ -227,6 +236,72 @@ const CarModels = () => {
               Automabil topish
             </button>
           </div>
+        </div>
+      </div>
+
+      {/* pagination */}
+
+      <div className="w-full flex justify-center py-5">
+        <div className="w-[1300px] space-y-5">
+          <h3 className="textStyle text-[20px] flex md:text-[25px] items-center">
+            Barcha <span className="text-[#E70A32]">{modelsArr.mark}</span>{" "}
+            automobillari
+          </h3>
+          <div className="grid grid-cols-1 gap-[40px] lg:gap-5 xl:gap-[50px] md:grid-cols-2 md:gap-6 lg:grid-cols-3 sm:grid-cols-2 anime">
+            {slicedCards.map((car) => {
+              return (
+                <Link
+                  to={`/mark/model/sotib-olish/${car.id}`}
+                  key={car.id}
+                  className="rounded space-y-3 relative"
+                >
+                  <div className="image_div relative">
+                    <div className="absolute flex space-x-3 top-3 left-3 z-10">
+                      <p className="bg-white text-[12px] py-[3px] px-[7px] fontStyle font-bold rounded-full">
+                        {car.numberOfusers} foidalanuvchi
+                      </p>
+                      <p className="bg-white text-[12px] py-[3px] px-[7px] fontStyle font-bold rounded-full">
+                        {car.year} yil
+                      </p>
+                    </div>
+                    <img
+                      src={car.image}
+                      alt={car.name}
+                      className="w-full transition-all hover:scale-110"
+                    />
+                  </div>
+                  <div className="space-y-5">
+                    <p className="fontSyle text-gray-500 text-[22px]">
+                      <span className="text-black font-bold">{car.name}</span> •{" "}
+                      {car.year}
+                    </p>
+                    <p className="text-gray-500 fontStyle text-[18px] font-bold">
+                      {car.year} yil , {car.probeg} km, Krosoveri {car.krosover}{" "}
+                      l, benzini {car.benzin} l.s
+                    </p>
+                    <div className="bg-gray-200 rounded p-3 hoverd">
+                      <div className="cost flex space-x-3 items-end">
+                        <p className="text-[26px] font-bold fontStyle">
+                          {car.reCost}$
+                        </p>
+                        <p className="text-[18px] font-bold fontStyle lineThrov">
+                          {car.cost}$
+                        </p>
+                      </div>
+                      <p className="fontStyle font-bold">
+                        Kredit:{" "}
+                        {car.kredit ? `${car.kredit}$` : "kredit ga Tolov yo'q"}
+                      </p>
+                    </div>
+                  </div>
+                </Link>
+              );
+            })}
+          </div>
+
+          {/* pagination navigate */}
+
+          <div></div>
         </div>
       </div>
       <Aksiyalar />
