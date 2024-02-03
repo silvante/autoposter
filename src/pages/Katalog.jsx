c13133aeed09d9ba0fde278680662b906c978d1a
Khamidov-M <mardonbekkhamidov69@gmail.com>
2024-02-03 18:35:46 +0500

    header is ready

--- src/pages/Katalog.jsx
+++ src/pages/Katalog.jsx
@@ -1,9 +1,17 @@
-import React from 'react'
+import React from "react";
 
 const Katalog = () => {
   return (
-    <div>Katalog</div>
-  )
-}
+    <div className="w-full flex justify-center">
+      <div className="w-[96%] xl:w-[1300px]">
+        <div className="w-full justify-between items-center">
+          <h3 className="text-black textStyle text-[20px] lg:text-[30px]">
+            Katalog
+          </h3>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default Katalog
+export default Katalog;
