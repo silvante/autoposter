ca50a73be3cf2ba804f8327a1b2bac83121dcf7c
Khamidov-M <mardonbekkhamidov69@gmail.com>
2024-02-07 21:44:25 +0500

    fen 11

--- src/components/Breadcrumps.jsx
+++ src/components/Breadcrumps.jsx
@@ -1,41 +1,40 @@
-import React from 'react'
-import { Link, useLocation } from 'react-router-dom'
+import React from "react";
+import { Link, useLocation } from "react-router-dom";
 
 const Breadcrumps = () => {
-    const location = useLocation()
-    // console.log(location);
-    const path = location.pathname.split("/");
-    // console.log(path);
-    const filterArr = path.filter((path) => path !== "");
-    // console.log(filterArr);
-    return (
-        <div className="w-full flex items-center justify-center">
-            <ul className='flex space-x-5 vercel my-5 w-[96%] xl:w-[1300px]'>
-                <li>
-                    <Link to={"/"} className='text-[#E70A32]'>
-                       Bosh sahifa
-                    </Link>
-                </li>
+  const location = useLocation();
+  const pathe = location.pathname.replaceAll("%20", " ");
+  location.pathname = pathe;
+  // console.log(location);
+  const path = location.pathname.split("/");
+  // console.log(path);
+  const filterArr = path.filter((path) => path !== "");
+  // console.log(filterArr);
+  return (
+    <div className="w-full flex items-center justify-center">
+      <ul className="flex space-x-5 vercel my-5 w-[96%] xl:w-[1300px]">
+        <li>
+          <Link to={"/"} className="text-[#E70A32]">
+            Bosh sahifa
+          </Link>
+        </li>
 
-                {
-                    filterArr.map((path, index) => {
-                        return (
-                            <li key={index}>
-                                
-                                {index !== filterArr.length - 1 ? (
-                                    <Link className='text-[#E70A32]' to={path}>
-                                        {`${path}`}
-                                    </Link>
-                                ) : (
-                                    path
-                                )}
-                            </li>
-                        )
-                    })
-                }
-            </ul>
-        </div>
-    )
-}
+        {filterArr.map((path, index) => {
+          return (
+            <li key={index}>
+              {index !== filterArr.length - 1 ? (
+                <Link className="text-[#E70A32]" to={path}>
+                  {`${path}`}
+                </Link>
+              ) : (
+                path
+              )}
+            </li>
+          );
+        })}
+      </ul>
+    </div>
+  );
+};
 
-export default Breadcrumps
+export default Breadcrumps;
